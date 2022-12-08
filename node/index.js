6e99aed28a4b6f34e9ae88c7c083feaecb12a7f3
DaniekBer <danielkbernardi@gmail.com>
2022-12-08 17:03:35 -0300

    mudancas

--- node/index.js
+++ node/index.js
@@ -10,13 +10,28 @@ const config = {
 
 const mysql = require('mysql');
 const connection = mysql.createConnection(config);
-const sql = `INSERT INTO people(name) values('Joao')`;
-connection.query(sql);
-connection.end();
+
+const sql = `INSERT INTO people(name) values('Daniel Bernardi');`
+connection.query(
+    sql
+);
 
 app.get('/', (req, res) => {
-   res.send('<h1>Daniel Full Cycle</h1>');
-});
+
+    var result = 'teste';
+    connection.query(
+        'SELECT * FROM `people`;',
+        function(err, results, fields) {
+            var a = '';
+            for(let person of results){
+                a = a.concat("<br/><p>Nome: " + person.name + "</p><br/>")
+            }
+            res.send('<h1>Full Cycle</h1><br/><strong>Lista das pessoas.</strong><br/>' + a)
+        }
+    );
+    connection.end()
+
+})
 
 app.listen(port, () => {
     console.log('Rodando a porta ' + port);
